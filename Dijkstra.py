da06f98c4981cbe0a05a41a2fc76fa59df63d5e1
qiangwennorge <qiang.wen.norge@gmail.com>
2017-04-23 20:26:32 +0200

    Dijkstra Shortest Path Algorithm with Weight

--- Dijkstra.py
+++ Dijkstra.py
@@ -5,6 +5,15 @@ Created on April 23 18:33 2017
 @author: qiangwennorge
 """
 
+def make_link(G, node1, node2, weight):
+    if node1 not in G:
+        G[node1] = {}
+    (G[node1])[node2] = weight
+    if node2 not in G:
+        G[node2] = {}
+    (G[node2])[node1] = weight
+    return G
+
 def shortest_dist_node(dist):
     best_node = 'undefined'
     best_value = 100000
@@ -18,15 +27,26 @@ def dijkstra(G,v):
     dist_so_far = {}
     dist_so_far[v] = 0
     final_dist = {}
-    while len(final_dist) < len(G)
+    while len(final_dist) < len(G):
         w = shortest_dist_node(dist_so_far)
         # lock it down!
         final_dist[w] = dist_so_far[w]
         del dist_so_far[w]
         for x in G[w]:
             if x not in final_dist:
-                new_dist = final_dist[w] + G[w][x]
-                if (x not in dist_so_far) or (new_dist < dist_so_far[x]):
-                    dist_so_far[x] = new_dist
+                if x not in dist_so_far:
+                    dist_so_far[x] = final_dist[w] + G[w][x]
+                elif final_dist[w] + G[w][x] < dist_so_far[x]:
+                    dist_so_far[x] = final_dist[w] + G[w][x]
     return final_dist
 
+(a,b,c,d,e,f,g) = ('A','B','C','D','E','F','G')
+triples = ((a,c,3),(c,b,10),(a,b,15),(d,b,9),(a,d,4),(d,f,7),(d,e,3),
+           (e,g,1),(e,f,5),(f,g,2),(b,f,1))
+
+G = {}
+
+for (i,j,k) in triples:
+    make_link(G,i,j,k)
+
+print dijkstra(G,a)

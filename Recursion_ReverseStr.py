4521c26d4d1c782637d2ff2cebcb981c204e6707
qiangwennorge <qiang.wen.norge@gmail.com>
2017-04-26 19:56:28 +0200

    Reverse String by Recursive Implementation

--- Recursion_ReverseStr.py
+++ Recursion_ReverseStr.py
@@ -12,3 +12,13 @@ def reverse_str(s):
         return reverse_str(s[1:]) + s[0]
 
 print reverse_str("alaska usa")
+
+def is_palindrome(s):
+    new_s = s.replace(' ','')
+    re_s = reverse_str(new_s)
+    if re_s == new_s:
+        return True
+    else:
+        return False
+
+print is_palindrome("live not on evil")
